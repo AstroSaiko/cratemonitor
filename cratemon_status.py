d7075c767129cfdf7365c54d2b1fefb51b8dae08
xtaldaq <xtaldaq@cmsuppixch.cern.ch>
2015-12-14 15:16:51 +0100

    almost done, just missing cronjob

--- cratemon_status.py
+++ cratemon_status.py
@@ -13,7 +13,7 @@ import rrdtool
 
 if __name__ == "__main__":
 
-    for crate in ['s1e02-27','s1e02-18','s1e02-10','s1e03-36','s1e03-27','s1e03-18']:
+    for crate in ['mch01','mch02']:
         print "{0}.rrd".format(crate)
         ret = rrdtool.fetch("{0}.rrd".format(crate), "AVERAGE", "-s -2min")
         print ret
