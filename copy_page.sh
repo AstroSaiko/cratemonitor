11ecf0f8680929e73c4a71467389e58845ce05a5
Georg Auzinger <georg.auzinger@cern.ch>
2015-12-15 11:57:59 +0100

    added script to copy images to web directory

---
+++ copy_page.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'Copying files for webpage!'
+export DIRNAME="/afs/cern.ch/user/g/gauzinge/www/cratemonitor/"
+echo 'moving files to' $DIRNAME
+cp /home/xtaldaq/monitoring/*.png $DIRNAME
