11b61fd1b15784095a4170424276b01c00183aa3
akornmay <birk.engegaard@cern.ch>
2017-04-05 15:57:54 +0200

    Progress on CU and PM monitoring

--- ipmimonitoring.py
+++ ipmimonitoring.py
@@ -8,17 +8,47 @@ import time
 
 
 line = []
-proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -P '' -T 0x82 -b 7 -B 0 -t 0xc2 sdr").split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
-#while True:
-#    out = proc.stdout.read(1)
-#    if out == '' and not proc.poll() == None:
-#        break
-#    if out != '':
-#        sys.stdout.write(out)
-#        sys.stdout.flush()
 
-(out, err) = proc.communicate()
-print out
-if err != '':
-    print err
+def getPMData(slot):
+    if slot == 1:
+        PM = "0xc2"
+    elif slot == 2:
+        PM = "0xc4"
+    elif slot == 3:
+        PM = "0xc6"
+    elif slot == 4:
+        PM = "0xc8"
+    else:
+        print "Please insert a valid slot (1-4)"
+        sys.exit() #Get standard exit codes here?
+    proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -P '' -T 0x82 -b 7 -B 0 -t {0} sdr".format(PM)).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+    (data, err) = proc.communicate()
+    #print out
+    if err != '':
+        print err
+        return -1
+    data = data.split('\n')
+    return data
+
+def getCUData(CU_index):
+    if CU_index == 1:
+        CU = "0xa8"
+    elif CU_index == 2:
+        CU = "0xaa"
+    else:
+        print "Please insert a valid index (1 or 2)"
+        sys.exit()
+    proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -P '' -T 0x82 -b 7 -B 0 -t {0} sdr".format(CU)).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+    (data, err) = proc.communicate()
+    #print out                                                                                                                                                                                                                               
+    if err != '':
+        print err
+        return -1
+    data = data.split('\n')
+    return data
+
+if __name__ == "__main__":
+    for i in [1,2,3,4]:
+        data = getCUData(i)
+        print data
 
