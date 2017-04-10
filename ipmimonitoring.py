cd3f000c2cd6b28236b0fbffc8b8bbfc0a4c6988
akornmay <birk.engegaard@cern.ch>
2017-04-10 11:42:19 +0200

    Added amc13 monitoring

--- ipmimonitoring.py
+++ ipmimonitoring.py
@@ -21,8 +21,10 @@ def getPMData(slot):
     proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -U '' -P '' -T 0x82 -b 7 -B 0 -t {0} sdr".format(PM)).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
     (data, err) = proc.communicate()
     if err != '':
-        print err
-        return -1
+        #if not "Get HPM.x Capabilities request failed, compcode = c9" in err:
+        if err != "Get HPM.x Capabilities request failed, compcode = c9\n":
+            print err
+            return -1
     data = data.split('\n')
     return data
 
@@ -37,8 +39,21 @@ def getCUData(CU_index):
     proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -U '' -P '' -T 0x82 -b 7 -B 0 -t {0} sdr".format(CU)).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
     (data, err) = proc.communicate()
     if err != '':
-        print err
-        return -1
+        #if not "Get HPM.x Capabilities request failed, compcode = c9" in err:
+        if err != "Get HPM.x Capabilities request failed, compcode = c9\n":    
+            print err
+            return -1
+    data = data.split('\n')
+    return data
+
+def getAMC13Data():
+    proc = subprocess.Popen(("ipmitool -H mch-e1a04-18 -U admin -P admin sdr entity 193.122").split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+    (data, err) = proc.communicate()
+    if err != '':
+        #if not "Get HPM.x Capabilities request failed, compcode = c9" in err:
+        if err != "Get HPM.x Capabilities request failed, compcode = c9\n":
+            print err
+            return -1
     data = data.split('\n')
     return data
 
@@ -52,6 +67,12 @@ if __name__ == "__main__":
     CUVoltages = []
     CUTemperatures = []
     fanSpeeds = []
+    
+    #for AMC13
+    #T2Temp = []
+    #amc1312V = []
+    #amc133V3 = []
+    #amc131V2 = []
 
     for i in [1, 2]:
 
@@ -168,18 +189,40 @@ if __name__ == "__main__":
         fanSpeeds.append(fan5)
         fanSpeeds.append(fan6)
 
-    #
+    
+    #for AMC13
+    amc13data = getAMC13Data()
+    if amc13data == -1:
+        print "amc13 error"
+    else:
+        for item in amc13data:
+            if "T2 Temp" in item:
+                T2Temp = item.strip().split(" ")[19]
+            elif "+12V" in item:
+                amc13_12V = item.strip().split(" ")[21]
+            elif "+3.3V" in item:
+                amc13_3V3 = item.strip().split(" ")[20]
+            elif "+1.2V" in item:
+                amc13_1V2 = item.strip().split(" ")[20]
+    
+    print ''
+    print "Data:"
+    print ''
     print "PMTemperatures: [PM1 tbrick-a, PM1 tbrick-b, PM1 t-base, PM2 trick-a, PM2 tbrick-b, PM2, t-base]"
-    print PMTemperatures
+    print "              ", PMTemperatures
     print "PMVoltages: [PM1 VIN, PM1 VOutA, PM1 VOutB, PM1 12V, PM1 3.3V, PM2 VIN, PM2 VOutA, PM2 VOutB, PM2 12V, PM2 3.3V]"
-    print PMVoltages
-
+    print "              ", PMVoltages
+    print ''
     print "CUVoltages: [CU1 3.3V, CU1 12V, CU1 12V_1, CU2 3.3V, CU2 12V, CU2 12V_1]"
-    print CUVoltages
+    print "            ", CUVoltages
     print "CUTemperatures: [CU1 LM75Temp, CU1 LM75Temp2, CU2 LM75Temp, CU2 LM75Temp2]"
-    print CUVoltages
+    print "             ", CUVoltages
     print "fanSpeeds: [CU1 fan1, CU1 fan2, CU1 fan3, CU1 fan4, CU1 fan5, CU1 fan6, CU2 fan1, CU2 fan2, CU3 fan3, C21 fan4, CU2 fan5, CU2 fan6]"
-    print fanSpeeds
+    print "           ", fanSpeeds
+    print ''
+    print "AMC13: [T2Temp, 12V, 3.3V, 1.2V]"
+    print "       ", [T2Temp, amc13_12V, amc13_3V3, amc13_1V2]
+    print ''
     
     #if everything is fine
     sys.exit(0)
